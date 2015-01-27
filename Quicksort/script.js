fd54b6204a76c8d164370c6965ce426983436ac1
George Bateman <george.bateman16@gmail.com>
2015-01-27 19:52:25 +0000

    Finish Quicksort
    Corrections, animations.

--- Quicksort/script.js
+++ Quicksort/script.js
@@ -4,11 +4,28 @@ $(document).ready(function() {
   $("#demo")
   .click(function() {
     $("#demo div")
-      .each(function() {
+      .each(function(n) {
         if ($(this).is(":hidden")) {
-          $(this).slideDown(500);
+          if (n & 1) {
+            $(this)
+              .css({right: "-100%", position: "relative"})
+              .show()
+              .animate({right: "50px"}, {duration: 200})
+              .animate({right: "0"});
+          } else {
+            $(this)
+              .css({left: "-100%", position: "relative"})
+              .show()
+              .animate({left: "50px"}, {duration: 200})
+              .animate({left: "0"});
+          }
           return false;
         }
       });
   });
+
+
+  $("body>div")
+  .prepend("<div style=\"edit-button\"></div>");
+  // onclick=\"this.parentNode.contentEditable = true;\"
 });
